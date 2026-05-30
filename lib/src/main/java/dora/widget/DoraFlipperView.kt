f68edadf5fac1796492499833e615b803f5bd56d
跟着哆啦学安卓 <71242257+dora4@users.noreply.github.com>
2026-05-30 21:13:39 +0700

    Update DoraFlipperView.kt

--- lib/src/main/java/dora/widget/DoraFlipperView.kt
+++ lib/src/main/java/dora/widget/DoraFlipperView.kt
@@ -14,6 +14,7 @@ import android.view.animation.TranslateAnimation
 import android.widget.TextSwitcher
 import android.widget.TextView
 import androidx.core.content.withStyledAttributes
+import dora.widget.flipperview.R
 import java.util.concurrent.LinkedBlockingQueue
 
 class DoraFlipperView @JvmOverloads constructor(
